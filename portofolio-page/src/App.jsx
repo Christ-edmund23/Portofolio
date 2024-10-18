792ee7f38da5651c71d326e72b5ea2171d450c6e
Christ-edmund23 <94790146+Christ-edmund23@users.noreply.github.com>
2024-10-19 01:36:17 +0700

    button dan deskripsi singkat

--- portofolio-page/src/App.jsx
+++ portofolio-page/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./Header.jsx";
 import { Avatar } from "@mui/material";
 import Background from "../src/assets/BG-Lake.jpg"
 import Profile from "../src/assets/Profile.jpeg"
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -11,7 +12,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 function App() {
     return (
         <>
-            <div className="w-full h-16 flex flex-col items-center p-4 bg-black">
+            <div className="w-full px-8 py-8 flex flex-col items-center p-4 bg-black">
                 <div className="ml-auto mr-20">
                     <nav>
                         <ul className="flex space-x-4 font-bold gap-4">
@@ -22,22 +23,45 @@ function App() {
                     </nav>
                 </div>
             </div>
-            <div style={{
+            <section id='home'>
+                <div style={{
                     backgroundImage: `url(${Background})`,
                     backgroundRepeat: "no-repeat",
                     backgroundSize: "cover",
-                    backgroundColor: "rgba(0, 0, 0, 0.7)", // Black with 50% transparency
+                    backgroundColor: "rgba(0, 0, 0, 0.7)", // Black with 70% transparency
                     backgroundBlendMode: "overlay", 
                 }}className="bg-gray-600 flex flex-row items-center text-white w-full justify-center pt-10 pb-10">
-                <div className="flex flex-col font-semibold w-1/2 text-4xl gap-2">
-                    <p>Hi,</p>
-                    <p>I'm <span className="text-sky-300">Christopher Edmund Haryanto</span></p>
-                    <p>a Data Analyst</p>
-                </div>
-                    <div className="flex flex-col w-1/4 ">
-                        <img src={Profile} alt="" className="rounded-full w-96" />
+                    <div className="flex flex-col w-1/2">
+                        <div className='text-3xl gap-2 font-semibold mb-5'>
+                            <p>Hello,</p>
+                            <p>I'm <span className="text-sky-300">Christopher Edmund Haryanto</span></p>
+                            <p>a Data Analyst</p>
+                        </div>
+                        <div className='text-sm text-wrap text-balance w-3/4 mb-4'>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                            </p>
+                        </div>
+                        <div>
+                            <button
+                                className='px-2 py-2 pl-4 pr-2 bg-sky-600 rounded-full flex items-center gap-1'
+                                onClick={() => {
+                                    document.getElementById('about-me').scrollIntoView({behavior:'smooth'})
+                                }}>
+                                About Me
+                                <ArrowDropDownIcon />
+                            </button>
+                        </div>
                     </div>
+                        <div className="flex flex-col w-1/4 ">
+                            <img src={Profile} alt="" className="rounded-full" />
+                        </div>
                 </div>
+            </section>
+            <section id='about-me'>
+
+            </section>
 
         </>
 
