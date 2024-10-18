750e9e059991ef95269a937b031d62f60f6418bf
Christ-edmund23 <94790146+Christ-edmund23@users.noreply.github.com>
2024-10-18 21:02:55 +0700

    nambahin profile sama nama

--- portofolio-page/src/App.jsx
+++ portofolio-page/src/App.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Header from "./Header.jsx";
 import { Avatar } from "@mui/material";
 import Background from "../src/assets/BG-Lake.jpg"
+import Profile from "../src/assets/Profile.jpeg"
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -8,28 +11,34 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 function App() {
     return (
         <>
-            <div className="fixed top-0 w-full h-16 flex flex-col items-center p-4">
+            <div className="w-full h-16 flex flex-col items-center p-4 bg-black">
                 <div className="ml-auto mr-20">
                     <nav>
                         <ul className="flex space-x-4 font-bold gap-4">
-                            <li> <a href="#" className="text-black hover:text-blue-500">Home </a></li>
-                            <li> <a href="#" className="text-black hover:text-blue-500">About Me</a></li>
-                            <li> <a href="#" className="text-black hover:text-blue-500">Contact</a></li>
+                            <li> <a href="#" className="text-white hover:text-blue-500">Home </a></li>
+                            <li> <a href="#" className="text-white hover:text-blue-500">About Me</a></li>
+                            <li> <a href="#" className="text-white hover:text-blue-500">Contact</a></li>
                         </ul>
                     </nav>
                 </div>
-                <div className="mt-60 flex flex-col items-center">
-                    <img src={Background} alt="" className="w-40 h-40 rounded-full" />
-                    <h2 className="text-xl font-bold mt-4 mb-2">Christopher Edmund Haryanto</h2>
-                    <h3 className="mb-4">Data Analyst</h3>
-                    <div className="flex flex-row gap-4">
-                        <FacebookIcon />
-                        <InstagramIcon />
-                        <LinkedInIcon />
+            </div>
+            <div style={{
+                    backgroundImage: `url(${Background})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundColor: "rgba(0, 0, 0, 0.7)", // Black with 50% transparency
+                    backgroundBlendMode: "overlay", 
+                }}className="bg-gray-600 flex flex-row items-center text-white w-full justify-center pt-10 pb-10">
+                <div className="flex flex-col font-semibold w-1/2 text-4xl gap-2">
+                    <p>Hi,</p>
+                    <p>I'm <span className="text-sky-300">Christopher Edmund Haryanto</span></p>
+                    <p>a Data Analyst</p>
+                </div>
+                    <div className="flex flex-col w-1/4 ">
+                        <img src={Profile} alt="" className="rounded-full w-96" />
                     </div>
                 </div>
-                
-            </div>
+
         </>
 
     );
